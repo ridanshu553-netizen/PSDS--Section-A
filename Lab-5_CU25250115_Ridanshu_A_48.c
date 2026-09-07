cec859a52769e99384565eeca0c06e46ec32acf8
ridanshu553-netizen <ridanshu553@gmail.com>
2026-09-07 09:28:23 +0530

    Comment out infix to postfix implementation in Lab-5_CU25250115_Ridanshu_A_48.c

--- Lab-5_CU25250115_Ridanshu_A_48.c
+++ Lab-5_CU25250115_Ridanshu_A_48.c
@@ -1,51 +1,51 @@
-//1. Infix to Postfix
-#include <stdio.h>
-#include <ctype.h>
+// //1. Infix to Postfix
+// #include <stdio.h>
+// #include <ctype.h>
 
-char s[100];
-int top = -1;
+// char s[100];
+// int top = -1;
 
-int pre(char c) {
-    if (c == '^') return 3;
-    if (c == '*' || c == '/') return 2;
-    if (c == '+' || c == '-') return 1;
-    return 0;
-}
+// int pre(char c) {
+//     if (c == '^') return 3;
+//     if (c == '*' || c == '/') return 2;
+//     if (c == '+' || c == '-') return 1;
+//     return 0;
+// }
 
-int main() {
-    char in[100], out[100], c;
-    int i, j = 0;
+// int main() {
+//     char in[100], out[100], c;
+//     int i, j = 0;
 
-    printf("Enter infix: ");
-    scanf("%s", in);
+//     printf("Enter infix: ");
+//     scanf("%s", in);
 
-    for (i = 0; in[i]; i++) {
-        c = in[i];
+//     for (i = 0; in[i]; i++) {
+//         c = in[i];
 
-        if (isalnum(c))
-            out[j++] = c;
-        else if (c == '(')
-            s[++top] = c;
-        else if (c == ')') {
-            while (s[top] != '(')
-                out[j++] = s[top--];
-            top--;
-        }
-        else {
-            while (top != -1 && pre(s[top]) >= pre(c))
-                out[j++] = s[top--];
-            s[++top] = c;
-        }
-    }
+//         if (isalnum(c))
+//             out[j++] = c;
+//         else if (c == '(')
+//             s[++top] = c;
+//         else if (c == ')') {
+//             while (s[top] != '(')
+//                 out[j++] = s[top--];
+//             top--;
+//         }
+//         else {
+//             while (top != -1 && pre(s[top]) >= pre(c))
+//                 out[j++] = s[top--];
+//             s[++top] = c;
+//         }
+//     }
 
-    while (top != -1)
-        out[j++] = s[top--];
+//     while (top != -1)
+//         out[j++] = s[top--];
 
-    out[j] = '\0';
+//     out[j] = '\0';
 
-    printf("Postfix: %s", out);
-    return 0;
-}
+//     printf("Postfix: %s", out);
+//     return 0;
+// }
 
 
 //2. Postfix Evaluation
